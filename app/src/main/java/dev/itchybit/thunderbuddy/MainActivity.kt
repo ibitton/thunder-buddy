4257b721140776f51949530d86bbc583c6ef7d90
Itamar Bitton <itamarb@ramm.co.za>
2024-04-27 01:26:37 +0200

    Setup FusedLocationClient

---
+++ app/src/main/java/dev/itchybit/thunderbuddy/MainActivity.kt
@@ -0,0 +1,63 @@
+package dev.itchybit.thunderbuddy
+
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val PERMISSIONS_REQUEST_LOCATION = 100
+    }
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setupLocationService()
+    }
+
+    private fun setupLocationService() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        if (checkPermissions()) {
+            requestLocation()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION), 0
+            )
+        }
+    }
+
+    private fun checkPermissions(): Boolean = ContextCompat.checkSelfPermission(
+        this, android.Manifest.permission.ACCESS_COARSE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSIONS_REQUEST_LOCATION -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                requestLocation()
+            } else {
+                Toast.makeText(this, "Location permission is required.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestLocation() {
+        fusedLocationClient.lastLocation.addOnSuccessListener {
+            Log.d("Main", "${it.latitude} ${it.longitude}")
+        }
+    }
+}
